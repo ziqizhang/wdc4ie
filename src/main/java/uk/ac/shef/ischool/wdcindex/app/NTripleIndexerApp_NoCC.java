a65e565cf35a9c242c8dad2b8a464fa29dae1233
zz <ziqizhang.email@gmail.com>
2019-07-22 13:45:42 +0100

    zz

--- src/main/java/uk/ac/shef/ischool/wdcindex/app/NTripleIndexerApp_NoCC.java
+++ src/main/java/uk/ac/shef/ischool/wdcindex/app/NTripleIndexerApp_NoCC.java
@@ -10,35 +10,86 @@ import uk.ac.shef.ischool.wdcindex.pcd.NTripleIndexerWorker_NoCC;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+import java.nio.charset.Charset;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/*
+/home/zz/Work/wdc4ie/resources/solr_wdc
+ */
 public class NTripleIndexerApp_NoCC {
     private static final Logger LOG = Logger.getLogger(NTripleIndexerApp_NoCC.class.getName());
 
     public static void main(String[] args) throws IOException {
+
+        /*String content="http://ok.com> http://ok.com> \"values\" .";
+        Pattern p = Pattern.compile("\\<(.*?)\\>");
+        Matcher m = p.matcher(content);
+
+        int prevE=0;
+        String composed="";
+        while(m.find()){
+            int s = m.start();
+            int e = m.end();
+            String v = m.group();
+            v=v.substring(1, v.length()-1).trim();
+
+            //before the match
+            composed+=content.substring(prevE, s);
+            //the match
+            composed+="<"+v+">";
+
+            prevE=e;
+
+            System.out.println();
+        }
+        composed+=content.substring(prevE);
+        System.exit(1);
+*/
         SolrClient urlCache=null;
-        boolean indexURL = args.length==4;
+        boolean indexURL = args.length==5;
         if (indexURL) {
             CoreContainer solrContainer = new CoreContainer(args[3]);
             solrContainer.load();
             urlCache = new EmbeddedSolrServer(solrContainer.getCore("url"));
         }
+        System.out.println("Use solr="+urlCache);
+
+        String tmpFolder = args[1] + "/tmp";
+        File tmpStore = new File(tmpFolder);
+        Set<String> processedJobs = new HashSet<>();
+        if (!tmpStore.exists())
+            tmpStore.mkdirs();
+        else {
+            for (File f : tmpStore.listFiles()) {
+                if (f.getName().endsWith(".job")) {
+                    List<String> processed=FileUtils.readLines(f, Charset.forName("utf-8"));
+                    LOG.info("loaded "+processed.size()+" already processed from "+f);
+                    for (String pf : processed)
+                        processedJobs.add(pf.split(",")[0]);
+                }
+            }
+            LOG.info("Total already processed files:" + processedJobs.size());
+        }
 
         List<String> gzFiles = FileUtils.readLines(new File(args[0]));
+        gzFiles.removeAll(processedJobs);
+        LOG.info("Total files to process:" + gzFiles.size());
         Collections.sort(gzFiles);
-        int size = Integer.valueOf(args[2]);
-        List<List<String>> parts=Lists.partition(gzFiles, size);
+        int size = Integer.valueOf(args[3]);
+        List<List<String>> parts=
+                Lists.partition(gzFiles, size);
+        LOG.info("Total threads to be created:" + parts.size());
 
 
         LOG.info("Initialisation completed.");
 
         ExecutorService executor = Executors.newFixedThreadPool(parts.size());
         for (int i = 0; i < parts.size(); i++) {
-            Runnable worker = new NTripleIndexerWorker_NoCC(i,
+            Runnable worker = new NTripleIndexerWorker_NoCC(i,Integer.valueOf(args[2]),
                     args[1],
                     parts.get(i), urlCache);
             executor.execute(worker);
--- src/main/java/uk/ac/shef/ischool/wdcindex/pcd/NTripleIndexerWorker_NoCC.java
+++ src/main/java/uk/ac/shef/ischool/wdcindex/pcd/NTripleIndexerWorker_NoCC.java
@@ -19,6 +19,8 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.zip.GZIPInputStream;
 
 /**
@@ -41,43 +43,136 @@ import java.util.zip.GZIPInputStream;
 
 public class NTripleIndexerWorker_NoCC implements Runnable {
     private SolrClient urlCore;
-    private int commitBatch = 5000;
+    private int commitBatch = 50000;
     private int id;
 
     private String outFolder;
     private String tmpFolder;
+    private Pattern p = Pattern.compile("\\<(.*?)\\>");
+
+    private int parserOption = 0; //0 is regex, 1 is nxParser
 
     private static final Logger LOG = Logger.getLogger(NTripleIndexerWorker_NoCC.class.getName());
 
     //private DB db;
     private List<String> gzFiles;
-    private Set<String> processedJobs = new HashSet<>();
 
-
-    public NTripleIndexerWorker_NoCC(int id,
+    public NTripleIndexerWorker_NoCC(int id, int parserOption,
                                      String outFolder,
                                      List<String> inputGZFiles,
-                                     SolrClient urlCore) throws IOException {
+                                     SolrClient urlCore) {
         this.id = id;
+        this.parserOption = parserOption;
         this.urlCore = urlCore;
 
         this.gzFiles = inputGZFiles;
         this.outFolder = outFolder;
         tmpFolder = outFolder + "/tmp";
-        File tmpStore = new File(tmpFolder);
-        if (!tmpStore.exists())
-            tmpStore.mkdirs();
-        else {
-            for (File f : tmpStore.listFiles()) {
-                if (f.getName().endsWith(".job")) {
-                    List<String> processed=FileUtils.readLines(f, Charset.forName("utf-8"));
-                    LOG.info("loaded "+processed.size()+" already processed from "+f);
-                    processedJobs.addAll(processed);
-                }
-            }
-            LOG.info("Total already processed files:" + processedJobs.size());
+    }
+
+    private String[] nxParser(String content) {
+        try {
+            String subject = null, predicate = null, object = null, source = null;
+            Node[] quads = NxParser.parseNodes(content);
+            if (quads.length < 4)
+                return null;
+            subject = quads[0].toString();
+            if (!(quads[1] instanceof Resource) || !(quads[3] instanceof Resource))
+                return null;
+            predicate = ((Resource) quads[1]).toURI().toString();
+            source = ((Resource) quads[3]).toURI().toString();
+
+            subject = subject + "|" + source;
+
+            if (predicate == null)
+                return null;
+            if (quads[2] instanceof Resource)
+                object = ((Resource) quads[2]).toURI().toString();
+            else
+                object = quads[2].toString();
+
+            return new String[]{subject, predicate, object, source};
+        } catch (Exception e) {
+            LOG.warn(String.format("\t\t\t\tThread " + id + " encountered illegal nquad: %s, due to error: %s",
+                    content, ExceptionUtils.getFullStackTrace(e)));
+            return null;
         }
     }
+
+    /**
+     *
+     */
+    private String lightClean(String content){
+
+        Matcher m = p.matcher(content);
+
+        int prevE=0;
+        String composed="";
+        while(m.find()){
+            int s = m.start();
+            int e = m.end();
+            String v = m.group();
+            v=v.substring(1, v.length()-1).trim();
+
+            //before the match
+            composed+=content.substring(prevE, s);
+            //the match
+            composed+="<"+v+">";
+
+            prevE=e;
+        }
+        composed+=content.substring(prevE);
+        return composed;
+    }
+
+    /**
+     * @param content
+     * @return the 3rd (index=2) element could be null, if it is not a uri
+     * @throws Exception
+     */
+    private String[] regexParser(String content) throws Exception {
+        //split by space, which is the separator for nquad format
+        if (content.endsWith(".")) {
+            content = content.substring(0, content.length() - 1).replaceAll("\\s+", " ").trim();
+        }
+        String[] parts = content.split("\\s+");
+
+        if (parts.length < 3) {
+            LOG.warn(String.format("\t\t\t\tThread " + id + " encountered illegal nquad: %s",
+                    content));
+            return null;
+        }
+        //check each part, the first, and second should be either nodes or uris, so they must not have space and complete in itself.
+        String subject = parts[0];
+        String predicate = parts[1];
+        String object=parts[2];
+
+        //is predicate legal?
+        if (!predicate.startsWith("<") || !predicate.endsWith(">")) {
+            LOG.warn(String.format("\t\t\t\tThread " + id + " encountered illegal nquad: %s",
+                    content));
+            return null;
+        }
+
+        predicate = trimBrackets(predicate);
+        subject = trimBrackets(subject);
+        if (object.startsWith("<")&&object.endsWith(">"))
+            object=trimBrackets(object);
+        else
+            object=null;
+
+        //check source. If there is one, it must be a complete URI to be legal and should be the last element in array
+        String source = parts[parts.length - 1];
+        if (parts.length < 4 || !source.startsWith("<") || !source.endsWith(">")) {
+            LOG.warn(String.format("\t\t\t\tThread " + id + " encountered illegal nquad: %s",
+                    content));
+            return null;
+        }
+        source = trimBrackets(source);
+
+        return new String[]{subject, predicate, object, source};
+    }
+
 
     private Scanner setScanner(String file) throws IOException {
         InputStream fileStream = new FileInputStream(file);
@@ -91,15 +186,11 @@ public class NTripleIndexerWorker_NoCC implements Runnable {
 
     public void run() {
         int countFiles = 0;
-        int countAlready=0;
-        LOG.info("THREAD " + id + " has " + gzFiles.size()+" to process.");
+        long countIllegalLines = 0;
+        LOG.info("THREAD " + id + " has " + gzFiles.size() + " to process.");
 
         for (String inputGZFile : gzFiles) {
-            if (processedJobs.contains(inputGZFile)) {
-                countAlready++;
-                LOG.info("THREAD " + id + " already processed " + countAlready+", "+inputGZFile);
-                continue;
-            }
+
             countFiles++;
             try {
                 /*db = DBMaker.fileDB(outFolder + "/tmp/wdc-url" + id + ".db")
@@ -132,61 +223,50 @@ public class NTripleIndexerWorker_NoCC implements Runnable {
                 Scanner inputScanner = setScanner(downloadTo.toString());
                 while (inputScanner.hasNextLine() && (content = inputScanner.nextLine()) != null) {
                     lines++;
-
+                    /*if (lines==1390000)
+                        System.out.println();*/
             /*
             Parsing the s, p, o, and source
-             */
+            */
+            //\[(.*?)\]
+                    content=lightClean(content);
+                    String[] spos;
+                    if (parserOption == 1) {
+                        spos = nxParser(content);
+                    } else {
+                        spos = regexParser(content);
+                    }
+                    if (spos == null) {
+                        LOG.info(String.format("\t\t thread " + id + " cannot parse line=%d: %s",
+                                lines, content));
+                        countIllegalLines++;
+                        continue;
+                    }
+
+                    String subject = spos[0], predicate = spos[1], object = spos[2], source = spos[3];
+
+                    URI sourceURL;
+
                     try {
-                        String subject = null, predicate = null, object = null, source = null;
-                        Node[] quads = NxParser.parseNodes(content);
-                        if (quads.length<4)
-                            continue;
-                        subject = quads[0].toString();
-                        if (!(quads[1] instanceof Resource) || !(quads[3] instanceof Resource))
-                            continue;
-                        predicate = ((Resource) quads[1]).toURI().toString();
-                        source = ((Resource) quads[3]).toURI().toString();
-
-                        subject = subject + "|" + source;
-
-                        if (predicate == null)
-                            continue;
-                        URI sourceURL;
-                        if (quads[2] instanceof Resource)
-                            object = ((Resource) quads[2]).toURI().toString();
-                        else
-                            object = quads[2].toString();
-
-                        try {
-                            sourceURL = new URI(source);
-                            incrementStats(sourceURL, new URI(predicate), object,
-                                    propFreq, classFreq, hostFreq, hostPropFreqDetail,
-                                    hostClassFreqDetail,
-                                    propInHostFreqDetail, classInHostFreqDetail);
-                        } catch (Exception e) {
-                            LOG.info(String.format("\t\t thread " + id + " illegal URI: %s",
-                                    source));
-                            continue;
-                        }
-
-                        if (urlCore != null)
-                            indexURL(sourceURL, urlCore, downloadTo.getName());
-
-
-                        lines++;
-                        if (lines % commitBatch == 0) {
-                            LOG.info(String.format("\t\t thread " + id + " processsed %d lines for file %s...", lines, inputGZFile));
-                            if (urlCore != null)
-                                urlCore.commit();
-                        }
-
-                    }catch (URISyntaxException |ParseException e){
-                        LOG.warn(String.format("\t\t\t\tThread " + id + " encountered illegal URI syntax for quad (skipped): %s",
-                                content, ExceptionUtils.getFullStackTrace(e)));
+                        sourceURL = new URI(source);
+                        incrementStats(sourceURL, predicate, object,
+                                propFreq, classFreq, hostFreq, hostPropFreqDetail,
+                                hostClassFreqDetail,
+                                propInHostFreqDetail, classInHostFreqDetail);
                     } catch (Exception e) {
-                        e.printStackTrace();
-                        LOG.warn(String.format("\t\tThread " + id + " encountered problem for quad (skipped): %s, %s",
-                                content, ExceptionUtils.getFullStackTrace(e)).replaceAll("\\n","|"));
+                        LOG.info(String.format("\t\t thread " + id + " cannot parse line %d due to illegal source URI: %s, error=%s",
+                                lines, source, ExceptionUtils.getFullStackTrace(e)));
+                        countIllegalLines++;
+                        continue;
+                    }
+
+                    if (urlCore != null)
+                        indexURL(sourceURL, urlCore, downloadTo.getName());
+
+                    if (lines % commitBatch == 0) {
+                        LOG.info(String.format("\t\t thread " + id + " processsed %d lines for file %s...", lines, inputGZFile));
+                        if (urlCore != null)
+                            urlCore.commit();
                     }
 
                 }
@@ -209,13 +289,14 @@ public class NTripleIndexerWorker_NoCC implements Runnable {
                             hostClassFreqDetail,
                             propInHostFreqDetail, classInHostFreqDetail
                     );
+                    PrintWriter p = new PrintWriter(new FileWriter(tmpFolder + "/" + id + ".job", true));
+                    p.println(inputGZFile+","+countIllegalLines+","+lines);
+                    p.close();
+                    countIllegalLines=0;
                 } catch (Exception e) {
                     LOG.info("\t thread " + id + " saving data error " + inputGZFile);
                     LOG.info("\t thread " + id + " error:" + ExceptionUtils.getFullStackTrace(e));
                 }
-                PrintWriter p = new PrintWriter(new FileWriter(tmpFolder + "/" + id + ".job", true));
-                p.println(inputGZFile);
-                p.close();
 
                 try {
                     if (urlCore != null)
@@ -236,7 +317,7 @@ public class NTripleIndexerWorker_NoCC implements Runnable {
             }
         }
 
-        LOG.info("Thread " + id + " indexing completed, prev_processed="+countAlready+", newly_processed="+countFiles+", total="+gzFiles.size());
+        LOG.info("Thread " + id + " indexing completed, newly_processed=" + countFiles + ", total=" + gzFiles.size());
     }
 
     private void save(String inputFile,
@@ -328,7 +409,7 @@ public class NTripleIndexerWorker_NoCC implements Runnable {
         writer.close();
     }
 
-    private void incrementStats(URI source, URI predicate, String object,
+    private void incrementStats(URI source, String predicate, String object,
                                 Map<String, Integer> propFreq,
                                 Map<String, Integer> classFreq,
                                 Map<String, Integer> hostFreq,
@@ -337,16 +418,16 @@ public class NTripleIndexerWorker_NoCC implements Runnable {
                                 Map<String, Map<String, Integer>> propInHostFreqDetail,
                                 Map<String, Map<String, Integer>> classInHostFreqDetail) throws MalformedURLException {
         String host = source.getHost();
-        updateCount(predicate.toString(), propFreq);
-        if (predicate.toString().equalsIgnoreCase("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")) {
+        updateCount(predicate, propFreq);
+        if (predicate.equalsIgnoreCase("http://www.w3.org/1999/02/22-rdf-syntax-ns#type") && object!=null) {
             updateCount(object, classFreq);
             updateCount(host, object, hostClassFreqDetail);
             updateCount(object, host, classInHostFreqDetail);
         }
         updateCount(host, hostFreq);
 
-        updateCount(host, predicate.toString(), hostPropFreqDetail);
-        updateCount(predicate.toString(), host, propInHostFreqDetail);
+        updateCount(host, predicate, hostPropFreqDetail);
+        updateCount(predicate, host, propInHostFreqDetail);
     }
 
     private void updateCount(String key, Map<String, Integer> map) {
